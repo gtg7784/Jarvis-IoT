d7c4a1c153d3e7dd8c244c96635fa08579020b17
Go Tae Geon <gtg7784@lab-c.me>
2019-09-06 02:53:42 +0900

    update: resolution

--- Yolo/yolo.py
+++ Yolo/yolo.py
@@ -165,7 +165,7 @@ if __name__ == '__main__':
 		# vid = cv2.VideoCapture(0)
 
 		while True:
-			_, frame = vs.read()
+			_, frame = vs.read()[0], vs.read()[1]
 			height, width = 480, 640
 
 			if count == 0:
