0c554ee94076f6a72a831ce80c363d5a535f23d5
Go Tae Geon <gtg7784@lab-c.me>
2019-09-06 02:11:07 +0900

    들여쓰기를 스페이스로 해서 에러가 생긴다구우우우?

--- Yolo/yolo_utils.py
+++ Yolo/yolo_utils.py
@@ -97,38 +97,38 @@ def infer_image(net, layer_names, height, width, img, colors, labels, FLAGS,
     return img, boxes, confidences, classids, idxs
 
 class VideoStream:
-	def __init__(self, src=0, usePiCamera=False, resolution=(320, 240),
-		framerate=32):
+    def __init__(self, src=0, usePiCamera=False, resolution=(320, 240),
+        framerate=32):
 		# check to see if the picamera module should be used
-		if usePiCamera:
+        if usePiCamera:
 			# only import the picamera packages unless we are
 			# explicity told to do so -- this helps remove the
 			# requirement of `picamera[array]` from desktops or
 			# laptops that still want to use the `imutils` package
-			from pivideostream import PiVideoStream
+            from pivideostream import PiVideoStream
  
 			# initialize the picamera stream and allow the camera
 			# sensor to warmup
-			self.stream = PiVideoStream(resolution=resolution,
-				framerate=framerate)
+            self.stream = PiVideoStream(resolution=resolution,
+                framerate=framerate)
  
 		# otherwise, we are using OpenCV so initialize the webcam
 		# stream
-		else:
-			self.stream = WebcamVideoStream(src=src)
-    
+        else:
+            self.stream = WebcamVideoStream(src=src)
+
     def starting(self):
-		# start the threaded video stream
-		return self.stream.starting()
+        # start the threaded video stream
+        return self.stream.starting()
  
-	def update(self):
+    def update(self):
 		# grab the next frame from the stream
-		self.stream.update()
+        self.stream.update()
  
-	def read(self):
+    def read(self):
 		# return the current frame
-		return self.stream.read()
+        return self.stream.read()
  
-	def stop(self):
+    def stop(self):
 		# stop the thread and release any resources
-		self.stream.stop()
+        self.stream.stop()
